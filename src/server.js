ad94fa172ea0c8c41d4cf170e85256be3b1e0b27
Giabao11082005 <hoanggiabao110820055@gmail.com>
2025-08-22 21:59:45 +0700

    init project

--- src/server.js
+++ src/server.js
@@ -1,13 +1,30 @@
-const express = require("express");
+import express from "express";
+import { mapOrder } from "~/utils/sorts.js";
 
 const app = express();
+
 const hostname = "localhost";
-const PORT = 3000;
+const port = 8017;
 
 app.get("/", (req, res) => {
-  res.send(`Hello Bao`);
+  // Test Absolute import mapOrder
+  console.log(
+    mapOrder(
+      [
+        { id: "id-1", name: "One" },
+        { id: "id-2", name: "Two" },
+        { id: "id-3", name: "Three" },
+        { id: "id-4", name: "Four" },
+        { id: "id-5", name: "Five" },
+      ],
+      ["id-5", "id-4", "id-2", "id-3", "id-1"],
+      "id"
+    )
+  );
+  res.end("<h1>Hello World!</h1><hr>");
 });
 
-app.listen(PORT, hostname, () => {
-  console.log(`Hi Bao. Server running at http://${hostname}:${PORT}`);
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello Trung Quan Dev, I am running at ${hostname}:${port}/`);
 });
