93fdcdd4602790d0c664f52da7539b41520b5911
Giabao11082005 <hoanggiabao110820055@gmail.com>
2025-08-27 21:05:19 +0700

    feat(board): get api board details

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,7 @@
 import { slugify } from "~/utils/formatters";
 import { boardModel } from "~/models/boardModel";
+import ApiError from "~/utils/ApiErrorUtil";
+import { StatusCodes } from "http-status-codes";
 
 const createNew = async (reqBody) => {
   try {
@@ -20,4 +22,16 @@ const createNew = async (reqBody) => {
   }
 };
 
-export const boardService = { createNew };
+const getDetails = async (boardId) => {
+  try {
+    const board = await await boardModel.getDetails(boardId);
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, "Board Not Found !");
+    }
+    return board;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const boardService = { createNew, getDetails };
